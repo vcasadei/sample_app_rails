a6aa9d0b4ab9532d86a0e544e5d283965bbcdcb3
Vitor Casadei <vitor.casadei@gmail.com>
2015-11-26 18:41:23 +0000

    Problems resolution

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -32,10 +32,19 @@ class UserTest < ActiveSupport::TestCase
   
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+                         first.last@foo.jp alice+bob@baz.cn ]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+  
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
   
@@ -47,7 +56,10 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "email saved on downcase" do
-    # do something
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
   
   test "password should be present (nonblank)" do
